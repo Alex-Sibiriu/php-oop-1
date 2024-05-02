8216378b86be26ecb7816dcae0bff2e0dd5d56d2
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-05-02 14:32:40 +0200

    [add] Bonus 1

--- Models/Movie.php
+++ Models/Movie.php
@@ -2,17 +2,18 @@
   class Movie {
     public $title;
     public $direction;
-    public $genre;
+    public $genres;
     public $release_date;
 
-    public function __construct(string $_title, string $_direction, string $_genre, int $_release_date) {
+    public function __construct(string $_title, string $_direction, array $_genres, int $_release_date) {
       $this->title = $_title;
       $this->direction = $_direction;
-      $this->genre = $_genre;
+      $this->genres = $_genres;
       $this->release_date = $_release_date;
     }
 
     public function getFullInfo() {
-      return "Title: $this->title, direction: $this->direction, genre: $this->genre, release date:  $this->release_date";
+      $genresString = implode(' - ', $this->genres);
+      echo "Title: $this->title, direction: $this->direction, genres: $genresString, release date:  $this->release_date";
     }
   }
--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
   require_once __DIR__ . '/Models/Movie.php';
 
-  $the_fall_guy = new Movie('The Fall Guy', 'David Leitch', 'action', 2024);
+  $the_fall_guy = new Movie('The Fall Guy', 'David Leitch', ['action', 'comedy'], 2024);
 
-  $the_garfield_movie = new Movie('The Garfield Movie', 'Mark Dindal', 'family', 2024);
+  $the_garfield_movie = new Movie('The Garfield Movie', 'Mark Dindal', ['family'], 2024);
 
   var_dump($the_fall_guy);
   var_dump($the_garfield_movie);
   var_dump($the_fall_guy->getFullInfo())
+
 ?>
 
 <!DOCTYPE html>
